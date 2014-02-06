29e0a57a20ee6b579e4d062b4a7b5096f29c9809
Michael A Lundsveen <michael@lundsveen.no>
2014-02-06 10:56:20 +0100

    Added header logo

--- header.php
+++ header.php
@@ -20,22 +20,13 @@
 <body <?php body_class(); ?>>
 	<?php do_action( 'before' ); ?>
 	
-<header id="masthead" class="site-header" role="banner">
+<header id="masthead" class="site-header col-sm-6 pull-left" role="banner">
 	<div class="container">
 		<div class="row">
-			<div class="site-header-inner col-sm-12">
-					
-				<?php $header_image = get_header_image();
-				if ( ! empty( $header_image ) ) { ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-					</a>
-				<?php } // end if ( ! empty( $header_image ) ) ?>
-				
-				
+			<div class="site-header-inner">
+									
 				<div class="site-branding">
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<h4 class="site-description"><?php bloginfo( 'description' ); ?></h4>
+					<img src="<?php echo get_stylesheet_directory_uri() . '/includes/img/logo.png'; ?>">
 				</div>
 						
 			</div>
@@ -46,7 +37,7 @@
 <nav class="site-navigation">		
 	<div class="container">
 		<div class="row">
-			<div class="site-navigation-inner col-sm-12">
+			<div class="site-navigation-inner col-sm-6 pull-right">
 				<div class="navbar navbar-default">
 					<div class="navbar-header">
 				    <!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
@@ -60,12 +51,12 @@
 				    <!-- Your site title as branding in the menu -->
 				    <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				  </div>
-
+                    
 			    <!-- The WordPress Menu goes here -->
 	        <?php wp_nav_menu(
                 array(
                     'theme_location' => 'primary',
-                    'container_class' => 'collapse navbar-collapse navbar-responsive-collapse',
+                    'container_class' => 'collapse navbar-collapse navbar-responsive-collapse col-sm-6',
                     'menu_class' => 'nav navbar-nav',
                     'fallback_cb' => '',
                     'menu_id' => 'main-menu',
